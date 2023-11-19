59f55078cf6260886ea04f7d1dd5c4df7ed1fbd0
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-19 01:48:08 +0100

    Routes

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
+import { Fragment } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from '../../layout/Header'
+import Home from '../../pages/Home'
+import Footer from '../../layout/Footer'
 
 const App = () => {
-  return <BrowserRouter></BrowserRouter>
+  return (
+    <Fragment>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default App
