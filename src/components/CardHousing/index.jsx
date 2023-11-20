6de04c818ed7e39e46962d226ba5e72ce207007b
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-20 10:25:08 +0100

    Home -> Conponent/CardHousing

---
+++ src/components/CardHousing/index.jsx
@@ -0,0 +1,3 @@
+const CardHousing = () => {}
+
+export default CardHousing
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from '../components/Banner'
+import CardHousing from '../components/CardHousing'
 
 const Home = () => {
   return (
     <main className="home container">
       <Banner name="Home" />
+      <CardHousing />
     </main>
   )
 }
