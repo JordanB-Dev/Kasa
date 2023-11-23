7b7e5fde85f68639e6cabdda8afb0b179baa4488
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-23 19:11:20 +0100

    Rating

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,5 +1,37 @@
+import StarImg from '../../assets/images/star.png'
+import EmptyStar from '../../assets/images/emptystar.png'
+
 const Rating = ({ data }) => {
   const { rating } = data
+
+  const accommodation = []
+  let star = true
+  for (let index = 0; index < 5; index++) {
+    if (index === parseInt(rating)) {
+      star = false
+    }
+    if (star === true) {
+      accommodation.push(
+        <img
+          key={index}
+          className="star"
+          src={StarImg}
+          alt={`${data?.rating}/5`}
+        />,
+      )
+    } else {
+      accommodation.push(
+        <img
+          key={index}
+          className="star"
+          src={EmptyStar}
+          alt={`${data?.rating}/5`}
+        />,
+      )
+    }
+  }
+
+  return <div className="rating">{accommodation}</div>
 }
 
 export default Rating
