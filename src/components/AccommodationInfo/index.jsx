06b170ab022992a354807efc11c122eab60e2a6a
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-23 18:54:12 +0100

    AccommodationInfo

--- src/components/AccommodationInfo/index.jsx
+++ src/components/AccommodationInfo/index.jsx
@@ -1,5 +1,15 @@
 const AccommodationInfo = ({ data }) => {
-  console.log(data)
+  const { title, location } = data
+
+  return (
+    <div className="accommodation_info container">
+      <div className="accommodation_info_container_left">
+        <h2>{title}</h2>
+        <p>{location}</p>
+        <div className="tag_list"></div>
+      </div>
+    </div>
+  )
 }
 
 export default AccommodationInfo
