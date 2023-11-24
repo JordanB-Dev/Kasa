6871fde0b962ed72ddc1ce9b6111a601b3ff19d9
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-24 13:18:28 +0100

    Update

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react'
 import { useLocation } from 'react-router-dom'
 import accommodation from '../assets/data/data.json'
 import AccommodationContent from '../components/AccommodationContent'
@@ -7,15 +6,11 @@ import Error from './Error'
 const Accommodation = () => {
   const { search } = useLocation()
   const idaccommodation = new URLSearchParams(search).get('id')
-  const data = accommodation.filter(
-    (accommodation) => accommodation.id.toString() === idaccommodation,
-  )[0]
-
-  return (
-    <Fragment>
-      {data ? <AccommodationContent data={data} /> : <Error />}
-    </Fragment>
+  const data = accommodation.find(
+    (accommodation) => accommodation.id === idaccommodation,
   )
+
+  return <>{data ? <AccommodationContent data={data} /> : <Error />}</>
 }
 
 export default Accommodation
