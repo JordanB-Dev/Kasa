c0066c598f6daffee430e173c460299b36af32b2
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-25 12:09:52 +0100

    TS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,13 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/assets'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
