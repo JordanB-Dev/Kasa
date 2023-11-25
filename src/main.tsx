9d4ed04ad69cf57bc1e09743d83c4da4d6e5cf02
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-11-25 12:11:18 +0100

    TS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './components/App'
 import './assets/styles/style.css'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
